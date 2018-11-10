b0e1c6a344bdea2f4bd2fde767bb116961a94ea0
Yuxuan Zhan <yz010@bucknell.edu>
2018-11-10 15:03:19 -0500

    snake class

--- src/finalproject/Snake.java
+++ src/finalproject/Snake.java
@@ -120,4 +120,8 @@ public class Snake {
     public boolean getDie() {
         return isDead;
     }
+
+    public Point getHead() {
+        return head;
+    }
 }
---
+++ src/finalproject/SnakeController.java
@@ -0,0 +1,40 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2018
+*
+* Name: NAMES of team members
+* Date: Nov 10, 2018
+* Time: 2:49:20 PM
+*
+* Project: csci205FinalProject
+* Package: finalproject
+* File: SnakeController
+* Description:
+*
+* ****************************************
+ */
+package finalproject;
+
+import javafx.concurrent.Task;
+
+/**
+ *
+ * @author yz010
+ */
+public class SnakeController {
+
+    private int score;
+
+    public SnakeController() {
+    }
+
+    class SnakeTask extends Task<Integer> {
+
+        @Override
+        protected Integer call() throws Exception {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+    }
+
+}
