081e785d147c1005fdf7017ee73b4aee98fe7481
Matt McLaughlin <mpm022@bucknell.edu>
2018-11-10 15:34:12 -0500

    argh

---
+++ src/gui/GameController.java
@@ -0,0 +1,33 @@
+package gui;
+
+/* *****************************************
+ * * CSCI205 - Software Engineering and Design
+ * * Fall 2018
+ * *
+ * * Name: Matt McLaughlin
+ * * Date: Nov 9, 2018
+ * * Time: 6:09:13 PM
+ * *
+ * * Project: csci205
+ * * Package:
+ * * File: GameController
+ * * Description:
+ * *
+ * * ****************************************
+ *  */
+/**
+ *  *
+ *   * @author mpm022
+ *    */
+public class GameController {//implements EventHandler<ActionEvent>{
+
+    private GameView theView;
+    private GameModel theModel;
+
+    public GameController(GameView theView, GameModel theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+    }
+
+}
+
