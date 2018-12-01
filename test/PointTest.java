2344dd7ee0a885754f3373eb451375f3af07c666
mpm022 <mpm022@bucknell.edu>
2018-12-01 16:50:00 -0500

    Add new file

---
+++ test/PointTest.java
@@ -0,0 +1,64 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2018
+*
+* Name: NAMES of team members
+* Date: Nov 29, 2018
+* Time: 5:23:07 PM
+*
+* Project: csci205FinalProject
+* Package: finalproject
+* File: PointTest
+* Description:
+*
+* ****************************************
+ */
+package finalproject;
+
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author yc013
+ */
+public class PointTest {
+
+    public PointTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of outOfMap method, of class Point.
+     */
+    @Test
+    public void testOutOfMap() {
+        System.out.println("outOfMap");
+        int mapSize = 200;
+        Point instance = new Point(0, 0);
+        boolean expResult = false;
+        boolean result = instance.outOfMap(mapSize);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of equals method.
+     */
+    @Test
+    public void testEquals() {
+        Point one = new Point(0, 0);
+        Point two = new Point(0, 0);
+        Boolean ekual = one.equals(two);
+        assertEquals(true, ekual);
+    }
+
+}
